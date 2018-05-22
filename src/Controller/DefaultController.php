9ce0822a90a48f97864e99eb41a170a2c11b5c63
hideway <inbox.martinez@gmail.com>
2018-05-22 18:09:44 +0200

    Adding Annotations

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response('<h1>HOMEPAGE</h1>');
+    }
+}
